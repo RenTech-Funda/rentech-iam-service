2d2ed867f556f426cae422e342998cb53a59562a
Diego Vilca <diegovilcatut@gmail.com>
2026-06-07 23:14:46 -0500

    Update OpenAPI server URL to use HTTPS

--- src/main/java/com/floweytech/agrotrack/iam/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/floweytech/agrotrack/iam/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -48,7 +48,7 @@ public class OpenApiConfiguration {
         openApi.addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .servers(List.of(
-                        new Server().url("agrotrack-iam-service-egazc5hqgxgnc2df.southeastasia-01.azurewebsites.net").description("Production"),
+                        new Server().url("https://agrotrack-iam-service-egazc5hqgxgnc2df.southeastasia-01.azurewebsites.net").description("Production"),
                         new Server().url("http://localhost:8080").description("Local")
                 ))                .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
